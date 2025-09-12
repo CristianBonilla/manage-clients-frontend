c36917d3ac0fa636dddc60377c8ddcbe438f95b7
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-09-12 12:38:29 -0500

    :tada: Start of manage-clients-frontend successfully

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mcf-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'manage-clients-frontend';
+}
